eb72b91a64d508875a56737fd9a7318655644db2
Frugtoy <borislesin@yandex.ru>
2020-12-08 13:09:03 +0300

    refixed console logs v2.1

--- first_laba/main.c
+++ first_laba/main.c
@@ -8,7 +8,7 @@
 char* line = {"________________________________________________________"};
 
 //funk list
-void get_exit_pid_ppid(){printf("\n\natexit has been started\n An id of the process (PID): %x\n An id of the parent process (PPID): %x\n%s\n",getpid(),getppid(), line);}
+void get_exit_pid_ppid(){printf("atexit has been started\n[%x]: An id of the process (PID): %x\n[%x]: An id of the parent process (PPID): %x\n\n",getpid(),getpid(),getpid(),getppid());}
 
 void fork_inf(pid_t pid){
 
@@ -16,21 +16,21 @@ void fork_inf(pid_t pid){
 
     switch(pid){
         case -1: 
-            printf("it's seems we have a problem with fork on child process\n%s\n", line);
+            printf("[%x]: it's seems we have a problem with fork on child process\n\n", getpid());
             break;
 
         case 0:
-            printf("getting an information from child brunch\n");
+            printf("[%x]: getting an information from child brunch\n", getpid());
             pid_t c_pid = getpid();
             pid_t c_ppid = getppid();
-            printf("    PID: %x\n   PPID: %x\n%s\n", c_pid, c_ppid,line);
+            printf("[%x]:  PID: %x\n[%x]: PPID: %x\n\n",getpid(),getpid(), c_pid, c_ppid);
             break;
 
         default:
-            printf("\n\ngetting an information from parent brunch:\n   PID:%x\n   CPID:%x\n%s\n",getpid(),pid, line);
+            printf("\n\n[%x]: getting an information from parent brunch:\n[%x]: PID:%x\n[%x]: CPID:%x\n\n",getpid(),getpid(),getpid(),getpid(),pid);
 
             int end_of_c_p = waitpid(pid,child_proc,0);
-            printf("\n\ninformation from brunch with PID(%x):proc with PID (%x) is dead\n%s\n",getpid(), end_of_c_p ,line);
+            printf("\n\n[%x]:process with PID (%x) is dead\n\n",getpid(), end_of_c_p);
             break;
     }
 }
@@ -41,6 +41,6 @@ int main(){
 
     pid_t pid =fork();//fork
     fork_inf(pid);
-    printf("\nInformation from brunch with PID(%x)",getpid());
+    printf("\n[%x]: ",getpid());
     return 0;
 }
