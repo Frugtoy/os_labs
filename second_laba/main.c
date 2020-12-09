4a748d13d0adf9ed9b3fcd7f183ebec3a9f68970
Frugtoy <borislesin@yandex.ru>
2020-12-09 21:38:35 +0300

    get the magik numbers out of my programm

--- second_laba/main.c
+++ second_laba/main.c
@@ -6,7 +6,7 @@
 #include<time.h>
 
 #define child_proc 0
-#define dev 0
+#define buff_size 15
 
 void write_string(const char* string , int *fd);
 char* read_string(int *fd);
@@ -18,21 +18,21 @@ void write_string(const char* string , int *fd) {//f[1] writing str to f[0] (chi
     lt = time(NULL);
     ptr = localtime(&lt);
     close(fd[0]);
-    write(fd[1],string,15);
+    write(fd[1],string,buff_size);
     close(fd[1]);
     printf("[%x]: ",getpid());
     printf("[%d:%d:%d] *writing str to child*\n",ptr->tm_hour, ptr->tm_min, ptr->tm_sec);
 
 }
 char* read_string(int *fd){//f[0] (child) reading str from f[1]
-    char *str = (char*)malloc(16);
+    char *str = (char*)malloc(buff_size + 1);
     struct tm *ptr;
     time_t lt;
     lt = time(NULL);
     ptr = localtime(&lt);
     
     close(fd[1]);
-    read(fd[0],str,16);
+    read(fd[0],str,buff_size);
     close(fd[0]);
     printf("[%x]: ",getpid());
     printf("[%d:%d:%d]: message:",ptr->tm_hour, ptr->tm_min, ptr->tm_sec);
