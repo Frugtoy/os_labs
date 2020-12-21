2aa120060fa8fff1cd5c01fc8e020ecc09caca77
Frugtoy <borislesin@yandex.ru>
2020-12-21 17:49:16 +0300

    second lab a)

--- second_laba/a/main.c
+++ second_laba/a/main.c
@@ -8,55 +8,50 @@
 #define child_proc 0
 #define buff_size 15
 
-void write_string(const char* string , int *fd);
-char* read_string(int *fd);
-
-
-void write_string(const char* string , int *fd) {//f[1] writing str to f[0] (child)
-    struct tm *ptr;
-    time_t lt;
-    lt = time(NULL);
-    ptr = localtime(&lt);
-    close(fd[0]);
-    write(fd[1],string,buff_size);
-    close(fd[1]);
-    printf("[%x]: ",getpid());
-    printf("[%d:%d:%d] *writing str to child*\n",ptr->tm_hour, ptr->tm_min, ptr->tm_sec);
-
-}
-char* read_string(int *fd){//f[0] (child) reading str from f[1]
-    char *str = (char*)malloc(buff_size + 1);
-    struct tm *ptr;
-    time_t lt;
-    lt = time(NULL);
-    ptr = localtime(&lt);
-    
-    close(fd[1]);
-    read(fd[0],str,buff_size);
-    close(fd[0]);
-    printf("[%x]: ",getpid());
-    printf("[%d:%d:%d]: message:",ptr->tm_hour, ptr->tm_min, ptr->tm_sec);
-
-return str;
-}
-
-int main(){
+void write_string( int *fd);
+void read_string(int *fd);
+time_t timer;
+int main() {
     int proc[2];
     pid_t pid;
-    
-
     pipe(proc);
     pid = fork();
 
     if(pid == child_proc){//child
         sleep(5);
-        char* str = read_string(proc);
-        printf(" %s\n",str);
+        read_string(proc);
     }
     else {//parent
-        write_string("second laba a)",proc);
+        
+        write_string(proc);
         int end_of_c_p = waitpid(pid,child_proc,0);
     }
 
 return 0;
+}
+
+void write_string( int *fd) {//f[1](parent) writing str to f[0] (child)
+  
+    long int data[2];
+ 
+    
+    data[0] = time(0); //замеряем
+    data[1] = getpid();
+    close(fd[0]);
+    printf("[%x] *writing my pid and data to child to child*\n",getpid());
+    write(fd[1],data,sizeof(long int)*2);
+    close(fd[1]);
+}
+
+void read_string(int *fd){//f[0] (child) reading str from f[1]
+    
+    long int data[2];
+    close(fd[1]);
+    read(fd[0], data, sizeof( long int ) * 2 );
+    close(fd[0]);
+    timer = time(0);//time of the proc
+    printf( "\n[%x][%s] ",getpid(),ctime(&timer));
+    timer = data[0];
+    printf("string from parrent: %s %lx ",ctime(&timer) , data[1]);
+    
 }
