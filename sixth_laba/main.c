ab12ee725b951a74e680c8d9d7e3fdc83d1030bf
Frugtoy <borislesin@yandex.ru>
2020-12-28 20:09:54 +0300

    fixed troubles with synchro

--- sixth_laba/main.c
+++ sixth_laba/main.c
@@ -2,16 +2,24 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<unistd.h>
+
 #define r_count 10
 #define w_count 1
 #define WRITER_IDX 10
+#define ASCII_SPACE_POS 32
+#define ASCII_TILDA_POS 126
+
 void* read_thread_arr( void* arg);
 void* write_thread_arr(void* arg);
+
 int get_random_val(int dist);
 
 int  counter = 0;
 pthread_rwlock_t rwlocker;
-int arr[r_count];
+char arr[r_count] = {"----------"};
+
+
+
 int main(){
 
     printf("[tid][arr]\n");   
@@ -20,28 +28,34 @@ int main(){
     
     pthread_rwlock_init(&rwlocker,NULL);
 
+    pthread_create(&pthread_arr[WRITER_IDX],NULL, write_thread_arr,NULL);
     for(int i = 0; i < r_count; i++){
          pthread_create(&pthread_arr[i],NULL,  read_thread_arr,NULL);
     }
-    pthread_create(&pthread_arr[WRITER_IDX],NULL, write_thread_arr,NULL);
+    
 
     for( int i = 0; i < r_count + w_count; i ++ ){
         pthread_join(pthread_arr[i], NULL);
     }
+    
+    
+    
     pthread_rwlock_destroy(&rwlocker);
     return 0;
 }
 
 void * write_thread_arr(void * arg){
     for(;;){
-        sleep(get_random_val(2));
+
         pthread_rwlock_wrlock(&rwlocker);
-        counter++;
-        printf("counter has been increased to [%d]\n",counter);
-        printf("[tid][arr]\n");
-        arr[get_random_val(10)] = counter;
-        sleep(get_random_val(3));
+        
+        	counter++;
+        	printf("counter has been increased to %d\n[tid][arr]\n",counter);
+        	arr[get_random_val(10)] = (get_random_val(ASCII_SPACE_POS)) + (ASCII_TILDA_POS -ASCII_SPACE_POS);
+        
         pthread_rwlock_unlock(&rwlocker);
+        
+        sleep(get_random_val(3));
     }  
 
     
@@ -50,6 +64,7 @@ void * write_thread_arr(void * arg){
 
 int get_random_val(int dist){
    int res =  (rand() +getpid()) % dist; 
+
     return res;
 }
 
@@ -57,13 +72,7 @@ void *  read_thread_arr(void* arg){
     
     for(;;){
         pthread_rwlock_rdlock(&rwlocker);
-       printf("[%02x] [",(unsigned)pthread_self());
-        for(int i =0; i<r_count; i++){
-            printf(" %d ",arr[i]);
-        }
-        printf("]\n");
-        fflush(stdout);
-      
+       	printf("[%lx] [%s] \n",(long)pthread_self(), arr);
         pthread_rwlock_unlock(&rwlocker);
           sleep(get_random_val(3));
     }
